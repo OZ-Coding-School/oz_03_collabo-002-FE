9592c03e191562845bd5ba489e8ca758e8d99737
Skyler85 <worstlie1@gmail.com>
2024-08-13 15:03:51 +0900

    Feat: 카드 및 카드목록 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import Home from './components/home/Home';
+import CardList from './components/common/CardList';
 
 function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       textColor: {
         primary: '#FFCD2A',
+        black: '#222222',
+        gray: '#c1c1c1',
+        red: '#ff2727',
       },
       backgroundColor: {
         primary: '#FFCD2A',
+        black: '#222222',
+        gray: '#c1c1c1',
+        red: '#ff2727',
       },
+    },
+    fontFamily: {
+      sans: ['NanumSquare', 'ui-sans-serif', 'system-ui'],
+      nanum: [
+        'NanumSquare',
+        'NanumSquareLight',
+        'NanumSquareBold',
+        'NanumSquareExtraBold',
+        'sans-serif',
+      ],
     },
   },
   plugins: [],
