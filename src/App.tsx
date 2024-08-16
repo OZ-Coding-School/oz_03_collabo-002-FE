08c723da2059f0d0140c9ff03726dee988d041c8
Skyler85 <worstlie1@gmail.com>
2024-08-16 10:19:29 +0900

    Feat: 회원정보 페이지 UI 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import Home from './components/home/Home';
-import MyPage from './pages/MyPage';
 import ClassDetail from './components/classDetail/ClassDetail';
 import ChargePage from './pages/ChargePage';
 import Admin from './components/admin/Admin';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
+import Account from './components/account/Account';
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/account" element={<Account />} />
             <Route path="/category/:classDetail_id" element={<ClassDetail />} />
             <Route path="/charge" element={<ChargePage />} />
             <Route path="admin" element={<Admin />} />
