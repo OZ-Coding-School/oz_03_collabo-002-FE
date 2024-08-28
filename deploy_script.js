8832189c16a61ba39f2454fd62cad6e6d5c05419
Skyler85 <worstlie1@gmail.com>
2024-08-29 05:50:39 +0900

    Update deploy_script

--- deploy_script.js
+++ deploy_script.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // NCP Object Storage 설정
-const endpoint = new AWS.Endpoint('http://custom-k-client-deploy.s3-website.kr.object.ncloudstorage.com');
+const endpoint = new AWS.Endpoint('https://kr.object.ncloudstorage.com');
 const region = 'kr-standard'; // NCP의 리전
 const accessKeyId = process.env.NCP_ACCESS_KEY_ID;
 const secretAccessKey = process.env.NCP_SECRET_ACCESS_KEY;
