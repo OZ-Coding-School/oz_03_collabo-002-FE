7182a02d1b5f13a59631f7c9af065fe338b056a3
parkminah123 <hello.git.kna9919@gmail.com>
2024-08-29 18:30:48 +0900

    Fix: api 호출로 변경

--- src/store/useClassStore.ts
+++ src/store/useClassStore.ts
@@ -5,18 +5,18 @@ import axios from '../api/axios';
 const useClassStore = create<ClassState>((set, get) => ({
   classes: [],
   filteredClasses: {},
-
   fetchClasses: async () => {
     try {
       const response = await axios.get(`/classes`);
-      //console.log('응답 상태 코드:', response.status);
-      //console.log(response.data);
-      // console.log('응답 헤더:', response.headers['content-type']);
-      if (response.data && Array.isArray(response.data)) {
-        set({ classes: response.data });
+      console.log(response.data);
+      console.log('응답 헤더:', response.headers['content-type']);
+
+      // 응답 데이터가 객체인지 확인하고, 그 안의 data 속성이 배열인지 확인
+      if (response.data && Array.isArray(response.data.data)) {
+        set({ classes: response.data.data }); // data 배열을 사용
       } else {
         console.error('Unexpected data format:', response.data);
-        //set({ classes: [] });
+        set({ classes: [] }); // 형식이 예상과 다를 경우 빈 배열로 초기화
       }
     } catch (error) {
       console.error('Error fetching data:', error);
