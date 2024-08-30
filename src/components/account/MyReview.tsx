0bf470cde1006fa351009e991acfb9bafdfc1b2f
Skyler85 <worstlie1@gmail.com>
2024-08-30 13:30:38 +0900

    Fix: 마이페이지 내 데이터 없는경우 예외처리

--- src/components/account/MyReview.tsx
+++ src/components/account/MyReview.tsx
@@ -1,9 +1,35 @@
-const MyReview = () => {
-  return (
-    <div>MyReview
-    
-    </div>
-  )
-}
+import { useEffect } from 'react';
+import useReviewStore from '../../store/useReviewStore';
+import ReviewItem from '../review/ReviewItem';
 
-export default MyReview
+const Review = () => {
+  const myReviews = useReviewStore((state) => state.myReviews);
+  const getMyReviews = useReviewStore((state) => state.getMyReviews);
+
+  useEffect(() => {
+    getMyReviews();
+  }, [getMyReviews]);
+
+  if (!myReviews)
+    return (
+      <div className="inline-flex w-full aspect-square text-gray-500">
+        <span className="m-auto font-extralight text-gray text-xl">
+          No data
+        </span>
+      </div>
+    );
+
+  return (
+    <div className="w-full px-6">
+      {myReviews.length > 0 ? (
+        myReviews.map((review) => (
+          <ReviewItem key={review.id} review={review} />
+        ))
+      ) : (
+        <p> No reviews available </p>
+      )}
+    </div>
+  );
+};
+
+export default Review;
--- src/store/useReviewStore.ts
+++ src/store/useReviewStore.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
 import { Review, ReviewAction, ReviewState } from '../type/review.type';
 import axios from '../api/axios';
+import { useUserStore } from './useUser';
 
 const useReviewStore = create<ReviewState & ReviewAction>()(
   immer((set) => ({
@@ -18,11 +19,12 @@ const useReviewStore = create<ReviewState & ReviewAction>()(
         console.log('Failed to get reviews: ', error);
       }
     },
-    getMyReviews: async (userId) => {
+    getMyReviews: async () => {
+      const user = useUserStore.getState().user;
       try {
-        const response = await axios.get('/reviews/');
+        const response = await axios.get('/reviews');
         const data: Review[] = response.data;
-        const filteredData = data.filter((item) => item.user.id === userId);
+        const filteredData = data.filter((item) => item.user.id === user?.id);
         set({ myReviews: filteredData });
       } catch (error) {
         console.log('Failed to get my reviews: ', error);
