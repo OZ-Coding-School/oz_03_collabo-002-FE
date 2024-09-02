1f09f5c5bea58a1b7d588abf0c96c420db00a42f
Skyler85 <worstlie1@gmail.com>
2024-09-02 21:01:51 +0900

    feat: 마이페이지 내 빈데이터 출력문구 수정

--- src/components/account/MyOrderList.tsx
+++ src/components/account/MyOrderList.tsx
@@ -71,8 +71,8 @@ const MyOrderList = () => {
         <ul className="relative w-full list-none px-6">
           {Object.keys(groupedOrders).length === 0 ? (
             <li className="inline-flex w-full aspect-square text-gray-500">
-              <span className="m-auto font-extralight text-gray text-xl">
-                No data
+              <span className="m-auto w-5/6 text-gray text-xl text-center">
+                {`Experience your own unique story and discover a new Korea :)`}
               </span>
             </li>
           ) : (
