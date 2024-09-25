d198b00c34c81d2943c09ae73a50b060c1ab4a95
Doona <worstlie1@gmail.com>
2024-09-26 02:31:24 +0900

    [Chore] env 출력코드 테스트

--- src/components/Login/Kakao.ts
+++ src/components/Login/Kakao.ts
@@ -1,10 +1,16 @@
 const KakaoClientId = import.meta.env.VITE_KAKAO_CLIENT_ID;
 const redirect_uri = import.meta.env.VITE_REDIRECT_URL;
-console.log('KakaoClientId:', KakaoClientId);
-console.log('redirect_uri:', redirect_uri);
-const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KakaoClientId}&
-redirect_uri=${redirect_uri}&response_type=code&state=kakao`;
+
+console.log('전체 환경 변수:', import.meta.env);
+console.log('VITE_KAKAO_CLIENT_ID:', KakaoClientId);
+console.log('VITE_REDIRECT_URL:', redirect_uri);
+
+const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KakaoClientId}&redirect_uri=${redirect_uri}&response_type=code&state=kakao`;
 
 export const handleKaKao = () => {
-  window.location.href = kakaoURL;
+  console.log('카카오 로그인 URL:', kakaoURL);
+  // 5초 후에 리다이렉션
+  setTimeout(() => {
+    window.location.href = kakaoURL;
+  }, 5000);
 };
