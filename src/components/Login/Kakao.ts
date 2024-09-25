7f739e90bdfd936cddcc3f25a3b396fd7758c7f5
Doona <worstlie1@gmail.com>
2024-09-26 02:13:15 +0900

    [Chore] env 출력코드 테스트

--- src/components/Login/Kakao.ts
+++ src/components/Login/Kakao.ts
@@ -1,5 +1,7 @@
 const KakaoClientId = import.meta.env.VITE_REST_API_KAKAO_CLIENT_ID;
 const redirect_uri = import.meta.env.VITE_REDIRECT_URL;
+console.log('KakaoClientId:', KakaoClientId);
+console.log('redirect_uri:', redirect_uri);
 const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KakaoClientId}&
 redirect_uri=${redirect_uri}&response_type=code&state=kakao`;
 
